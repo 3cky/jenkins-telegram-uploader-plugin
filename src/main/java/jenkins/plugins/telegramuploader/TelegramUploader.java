a5341f6000fdfeba58c29de48ec0e2fd7e79d5bc
Victor Antonovich <v.antonovich@gmail.com>
2020-03-26 17:55:01 +0400

    Add "Test Connection" button to plugin advanced settings

--- src/main/java/jenkins/plugins/telegramuploader/TelegramUploader.java
+++ src/main/java/jenkins/plugins/telegramuploader/TelegramUploader.java
@@ -33,6 +33,7 @@ import java.net.URI;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
@@ -58,6 +59,7 @@ import org.kohsuke.stapler.StaplerRequest;
 import hudson.AbortException;
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.Functions;
 import hudson.Launcher;
 import hudson.Util;
 import hudson.model.AbstractProject;
@@ -69,6 +71,7 @@ import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
 import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
 import jenkins.util.VirtualFile;
 import net.sf.json.JSONObject;
@@ -188,39 +191,24 @@ public class TelegramUploader extends Notifier implements SimpleBuildStep {
         TelegramUploaderDescriptor descriptor = getDescriptor();
 
         HttpHost httpProxy = null;
-
-        CredentialsProvider httpProxyCredsProvider = new BasicCredentialsProvider();
-
-        String httpProxyUri = descriptor.getHttpProxyUri();
-        if (httpProxyUri != null && !httpProxyUri.isEmpty()) {
-            try {
-                URI proxyUri = URI.create(httpProxyUri);
-                httpProxy = new HttpHost(proxyUri.getHost(), proxyUri.getPort(),
-                        proxyUri.getScheme());
-                String httpProxyUser = descriptor.getHttpProxyUser();
-                if (httpProxyUser != null && !httpProxyUser.trim().isEmpty()) {
-                    httpProxyCredsProvider.setCredentials(
-                            new AuthScope(proxyUri.getHost(), proxyUri.getPort()),
-                            new UsernamePasswordCredentials(httpProxyUser.trim(),
-                                    descriptor.getHttpProxyPassword()));
-                }
-            } catch (Exception e) {
-                fail(logger, "Can't set up HTTP proxy: " + e.getMessage());
-                return;
-            }
+        try {
+            httpProxy = getHttpProxy(descriptor.getHttpProxyUri());
+        } catch (Exception e) {
+            fail(logger, "Can't set up HTTP proxy: " + e.getMessage());
+            return;
         }
 
-        try (CloseableHttpClient httpClient = HttpClients.custom()
-                .setDefaultCredentialsProvider(httpProxyCredsProvider).build()) {
+        try (CloseableHttpClient httpClient = getHttpClient(httpProxy,
+                descriptor.getHttpProxyUser(), descriptor.getHttpProxyPassword())) {
             for (String artifact : artifacts) {
                 VirtualFile artifactVirtualFile = artifactsRoot.child(artifact);
                 File artifactFile = new File(artifactVirtualFile.toURI());
                 logger.println("Uploading artifact '" + artifact + "' to the Telegram chat "
                         + this.chatId);
                 try {
-                    String response = sendFile(httpClient, httpProxy, descriptor.getBotToken(),
-                            expandedCaption, artifactFile);
-                    if (response.indexOf("\"ok\":true") < 0) {
+                    String response = sendTelegramFile(httpClient, httpProxy,
+                            descriptor.getBotToken(), expandedCaption, artifactFile);
+                    if (!isTelegramResponseOk(response)) {
                         fail(logger, "Error while uploading artifact '" + artifact
                                 + "' to Telegram chat " + this.chatId + ", response: " + response);
                         return;
@@ -243,8 +231,64 @@ public class TelegramUploader extends Notifier implements SimpleBuildStep {
         logger.println(message);
     }
 
-    public String sendFile(HttpClient httpClient, HttpHost httpProxy,
-            String token, String fileCaption, File file) throws IOException {
+    private static HttpHost getHttpProxy(String httpProxyUri) {
+        HttpHost httpProxy = null;
+        if (httpProxyUri != null && !httpProxyUri.isEmpty()) {
+            URI proxyUri = URI.create(httpProxyUri);
+            httpProxy = new HttpHost(proxyUri.getHost(), proxyUri.getPort(), proxyUri.getScheme());
+        }
+        return httpProxy;
+    }
+
+    private static CloseableHttpClient getHttpClient(HttpHost proxyHost, String httpProxyUser,
+            String httpProxyPassword) {
+        CredentialsProvider httpProxyCredsProvider = new BasicCredentialsProvider();
+        if (proxyHost != null && httpProxyUser != null && !httpProxyUser.trim().isEmpty()) {
+            httpProxyCredsProvider.setCredentials(
+                    new AuthScope(proxyHost.getHostName(), proxyHost.getPort()),
+                    new UsernamePasswordCredentials(httpProxyUser.trim(), httpProxyPassword));
+        }
+        return HttpClients.custom().setDefaultCredentialsProvider(httpProxyCredsProvider).build();
+    }
+
+    private static ResponseHandler<String> getHttpResponseHandler() {
+        return new ResponseHandler<String>() {
+            @Override
+            public String handleResponse(HttpResponse response)
+                    throws ClientProtocolException, IOException {
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode >= 200 && statusCode < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity) : null;
+                }
+                String status = Integer.toString(statusCode);
+                String reason = response.getStatusLine().getReasonPhrase();
+                if (reason != null) {
+                    status += " (" + reason + ")";
+                }
+                throw new ClientProtocolException("Unexpected response status: " + status);
+            }
+        };
+    }
+
+    private static boolean isTelegramResponseOk(String response) {
+        return response.indexOf("\"ok\":true") >= 0;
+    }
+
+    private static String sendTelegramRequest(HttpClient httpClient, HttpHost httpProxy,
+            String botToken, String botMethod, HttpEntity botData) throws IOException {
+        RequestConfig requestConfig = (httpProxy == null) ? RequestConfig.DEFAULT :
+            RequestConfig.custom().setProxy(httpProxy).build();
+        String requestUri = String.format("https://api.telegram.org/bot%s/%s", botToken, botMethod);
+        HttpUriRequest request = RequestBuilder.post(requestUri)
+                .setEntity(botData)
+                .setConfig(requestConfig)
+                .build();
+        return httpClient.execute(request, getHttpResponseHandler());
+    }
+
+    public String sendTelegramFile(HttpClient httpClient, HttpHost httpProxy,
+            String botToken, String fileCaption, File file) throws IOException {
         // Build multipart upload request
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
         builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
@@ -264,37 +308,26 @@ public class TelegramUploader extends Notifier implements SimpleBuildStep {
 
         HttpEntity data = builder.build();
 
-        // Build HTTP request and assign multipart upload data
-        RequestConfig requestConfig = (httpProxy == null) ? RequestConfig.DEFAULT :
-            RequestConfig.custom().setProxy(httpProxy).build();
-        HttpUriRequest request = RequestBuilder
-                .post(String.format("https://api.telegram.org/bot%s/sendDocument", token))
-                .setEntity(data)
-                .setConfig(requestConfig)
-                .build();
+        return sendTelegramRequest(httpClient, httpProxy, botToken, "sendDocument", data);
+    }
 
-        // Create a custom response handler
-        ResponseHandler<String> responseHandler = response -> {
-            int statusCode = response.getStatusLine().getStatusCode();
-            if (statusCode >= 200 && statusCode < 300) {
-                HttpEntity entity = response.getEntity();
-                return entity != null ? EntityUtils.toString(entity) : null;
+    public static void checkTelegramConnection(String botToken, String httpProxyUri,
+            String httpProxyUser, String httpProxyPassword)
+                    throws ClientProtocolException, IOException {
+        HttpHost httpProxy = getHttpProxy(httpProxyUri);
+        try (CloseableHttpClient httpClient = getHttpClient(httpProxy,
+                httpProxyUser, httpProxyPassword)) {
+            String response = sendTelegramRequest(httpClient, httpProxy,
+                    botToken, "getUpdates", null);
+            if (!isTelegramResponseOk(response)) {
+                throw new ClientProtocolException(response);
             }
-            String status = Integer.toString(statusCode);
-            String reason = response.getStatusLine().getReasonPhrase();
-            if (reason != null) {
-                status += " (" + reason + ")";
-            }
-            throw new ClientProtocolException("Unexpected response status: " + status);
-        };
-
-        return httpClient.execute(request, responseHandler);
+        }
     }
 
     @Symbol("telegramUploader")
     @Extension
     public static final class TelegramUploaderDescriptor extends BuildStepDescriptor<Publisher> {
-
         private String botToken;
         private String httpProxyUri;
         private String httpProxyUser;
@@ -343,6 +376,27 @@ public class TelegramUploader extends Notifier implements SimpleBuildStep {
             return FormValidation.ok();
         }
 
+        public FormValidation doTestConnection(
+                @QueryParameter("botToken") String token,
+                @QueryParameter("httpProxyUri") String proxyUri,
+                @QueryParameter("httpProxyUser") String proxyUser,
+                @QueryParameter("httpProxyPassword") String proxyPassword) throws IOException {
+            final Jenkins jenkins = Jenkins.get();
+            if (jenkins == null) {
+                throw new IOException("Jenkins instance is not ready");
+            }
+            jenkins.checkPermission(Jenkins.ADMINISTER);
+
+            try {
+                TelegramUploader.checkTelegramConnection(token, proxyUri, proxyUser, proxyPassword);
+            } catch (Exception e) {
+                return FormValidation.errorWithMarkup("<p>Can't connect to Telegram!</p><pre>" +
+                        Util.escape(Functions.printThrowable(e)) + "</pre>");
+            }
+
+            return FormValidation.ok("Successfully connected to Telegram!");
+        }
+
         @Override
         public boolean configure(StaplerRequest req, JSONObject json) throws FormException {
             JSONObject config = json.getJSONObject("telegram-uploader");
@@ -370,5 +424,4 @@ public class TelegramUploader extends Notifier implements SimpleBuildStep {
             return httpProxyPassword;
         }
     }
-
 }
